12c1298608acfc91938c1c4c22970eba31aff1fa
vivek <vivek.reck@gmail.com>
2021-06-26 01:31:32 +0530

    adding cors

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const routs = require('./router/app')
 
+const cors = require('cors')
+
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
